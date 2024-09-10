b53bd50376b93478b2f6b4d210a7271826df6957
Ari Davidson <ardavidson@students.chccs.k12.nc.us>
2024-09-10 09:04:48 -0400

    basic calculator works on streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,93 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Jump Rope Freestyle Score Calculator!")
+st.write("Input difficulty levels separated by commas")
+st.write("(Example: 3, 5, 6)")
+minPresentationPercent = 0.60
+minEntertainmentPercent = minPresentationPercent * 0.25
+minExecutionPercent = minPresentationPercent * 0.25
+minMusicalityPercent = minPresentationPercent * 0.20
+minCreativityPercent = minPresentationPercent * 0.15
+minVarietyPercent = minPresentationPercent * 0.15
+
+
+#equation taken from IJRU for calculating the point value per level
+def getPointValue(level):
+    if level!= 0:
+        pointValue = round(0.1 * (1.5**level), 2)
+    else:
+        pointValue = 0
+    return pointValue
+
+
+#cycles through all tricks and adds difficulty together
+def calculateDifficulty(input):
+    totalDifficultyRaw = 0
+    for trick in input:
+        totalDifficultyRaw = round(
+            (totalDifficultyRaw + getPointValue(int(trick))), 2)
+    return (totalDifficultyRaw)
+
+
+#calculates the minimum and maximum with presentation and it's parts
+def calculatePresentation(totalScore):
+    maxWithPresentation = round(
+        (totalScore + (totalScore * minPresentationPercent)), 2)
+    minWithPresentation = round((totalScore * minPresentationPercent), 2)
+    maxWithEntertainment = round(
+        (totalScore + (totalScore * minEntertainmentPercent)), 2)
+    minWithEntertainment = round(
+        (totalScore - (totalScore * minEntertainmentPercent)), 2)
+    maxWithExecution = round((totalScore + (totalScore * minExecutionPercent)),
+                             2)
+    minWithExecution = round((totalScore - (totalScore * minExecutionPercent)),
+                             2)
+    maxWithMusicality = round(
+        (totalScore + (totalScore * minMusicalityPercent)), 2)
+    minWithMusicality = round(
+        (totalScore - (totalScore * minMusicalityPercent)), 2)
+    maxWithCreativity = round(
+        (totalScore + (totalScore * minCreativityPercent)), 2)
+    minWithCreativity = round(
+        (totalScore - (totalScore * minCreativityPercent)), 2)
+    maxWithVariety = round((totalScore + (totalScore * minVarietyPercent)), 2)
+    minWithVariety = round((totalScore - (totalScore * minVarietyPercent)), 2)
+
+    return ([
+        maxWithPresentation, minWithPresentation, maxWithEntertainment,
+        minWithEntertainment, maxWithExecution, minWithExecution,
+        maxWithMusicality, minWithMusicality, maxWithCreativity,
+        minWithCreativity, maxWithVariety, minWithVariety
+    ])
+
+
+#takes input and turns it into a list of tricks
+def editInput(text):
+    text.replace(", ", ",")
+    text.replace(";", ",")
+    text.replace("; ", ",")
+    text.replace(".", ",")
+    text.replace(". ", ",")
+    trickList = text.split(",")
+    return trickList
+
+
+#prints the output
+def printOutput(totalDifficultyRaw, presentation):
+    st.write("Total Raw Difficulty:", totalDifficultyRaw)
+    st.write("All Presentation:", presentation[0], ", ", presentation[1])
+    st.write("Just Entertainment:", presentation[2], ", ", presentation[3])
+    st.write("Just Execution:", presentation[4], ", ", presentation[5])
+    st.write("Just Musicality:", presentation[6], ",", presentation[7])
+    st.write("Just Creativity:", presentation[8], ",", presentation[9])
+    st.write("Just Variety:", presentation[10], ", ", presentation[11])
+
+input = str(st.text_input(""))
+# Checks if there is an input and if it works
+if(input!= ""):
+    try:
+        difficulty = calculateDifficulty(editInput(input))
+        presentation = calculatePresentation(difficulty)
+        printOutput(difficulty, presentation)
+    except ValueError:
+        st.write("Please check your input and make sure it follows the example, something isn't right!")
