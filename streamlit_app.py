2ae8e60a5ee031357158b8f47ab8d6372bee6e2d
Ari Davidson <ardavidson@students.chccs.k12.nc.us>
2024-09-12 12:53:39 -0400

    added custom presentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,18 @@
 import streamlit as st
 import pandas as pd
+
 st.title("Jump Rope Freestyle Score Calculator!")
 url = "https://rules.ijru.sport/technical-manual/calculations/freestyle/single-rope"
 st.subheader("Scoring Calculations and Rules Taken From [IJRU Rulebook 4.0.0](%s)" % url)
 st.write("Developed and Maintained By Ari Davidson")
 
 
-minPresentationPercent = 0.60
-minEntertainmentPercent = minPresentationPercent * 0.25
-minExecutionPercent = minPresentationPercent * 0.25
-minMusicalityPercent = minPresentationPercent * 0.20
-minCreativityPercent = minPresentationPercent * 0.15
-minVarietyPercent = minPresentationPercent * 0.15
+maxPresentationAffect = 0.60
+minEntertainmentPercent = maxPresentationAffect * 0.25 # 15% - 0.15
+minExecutionPercent = maxPresentationAffect * 0.25 # 15% - 0.15
+minMusicalityPercent = maxPresentationAffect * 0.20 # 12% - 0.12 
+minCreativityPercent = maxPresentationAffect * 0.15 # 9% - 0.09
+minVarietyPercent = maxPresentationAffect * 0.15 # 9% - 0.09
 
 
 #equation taken from IJRU for calculating the point value per level
@@ -42,25 +43,16 @@ def calculateOldDifficulty(input):
 
 #calculates the minimum and maximum with presentation and it's parts
 def calculatePresentation(totalScore):
-    maxWithPresentation = round(
-        (totalScore + (totalScore * minPresentationPercent)), 2)
-    minWithPresentation = round(totalScore - (totalScore * minPresentationPercent), 2)
-    maxWithEntertainment = round(
-        (totalScore + (totalScore * minEntertainmentPercent)), 2)
-    minWithEntertainment = round(
-        (totalScore - (totalScore * minEntertainmentPercent)), 2)
-    maxWithExecution = round((totalScore + (totalScore * minExecutionPercent)),
-                             2)
-    minWithExecution = round((totalScore - (totalScore * minExecutionPercent)),
-                             2)
-    maxWithMusicality = round(
-        (totalScore + (totalScore * minMusicalityPercent)), 2)
-    minWithMusicality = round(
-        (totalScore - (totalScore * minMusicalityPercent)), 2)
-    maxWithCreativity = round(
-        (totalScore + (totalScore * minCreativityPercent)), 2)
-    minWithCreativity = round(
-        (totalScore - (totalScore * minCreativityPercent)), 2)
+    maxWithPresentation = round((totalScore + (totalScore * maxPresentationAffect)), 2)
+    minWithPresentation = round(totalScore - (totalScore * maxPresentationAffect), 2)
+    maxWithEntertainment = round((totalScore + (totalScore * minEntertainmentPercent)), 2)
+    minWithEntertainment = round((totalScore - (totalScore * minEntertainmentPercent)), 2)
+    maxWithExecution = round((totalScore + (totalScore * minExecutionPercent)), 2)
+    minWithExecution = round((totalScore - (totalScore * minExecutionPercent)), 2)
+    maxWithMusicality = round((totalScore + (totalScore * minMusicalityPercent)), 2)
+    minWithMusicality = round((totalScore - (totalScore * minMusicalityPercent)), 2)
+    maxWithCreativity = round((totalScore + (totalScore * minCreativityPercent)), 2)
+    minWithCreativity = round((totalScore - (totalScore * minCreativityPercent)), 2)
     maxWithVariety = round((totalScore + (totalScore * minVarietyPercent)), 2)
     minWithVariety = round((totalScore - (totalScore * minVarietyPercent)), 2)
     dict = {"presMax": maxWithPresentation,
@@ -77,6 +69,15 @@ def calculatePresentation(totalScore):
         "varietyMin": minWithVariety}
     return (dict)
 
+def calculateEditedPresentation(totalScore, entertainment, execution, musicality, creativity, variety):
+    print(totalScore)
+    editedScore = (totalScore 
+    + (totalScore*execution)
+    + (totalScore*entertainment)
+    + (totalScore*musicality)
+    + (totalScore*creativity)
+    + (totalScore*variety))
+    return(round(editedScore, 2))
 
 #takes input and turns it into a list of tricks
 def editInput(text):
@@ -101,21 +102,31 @@ def printOutput( presentation):
     return( output)
 st.header("Calculator")
 input = str(st.text_input(label="", placeholder="Input difficulty levels(2, 3, 4, etc.)"))
+with st.popover("Customize Presentation"):
+    entertainment = st.slider("Entertainment", min_value=-0.15, max_value=0.15, value=0.00)
+    execution = st.slider("Execution", min_value=-0.15, max_value=0.15, value=0.00)
+    musicality = st.slider("Musicality", min_value=-0.12, max_value=0.12, value=0.00)
+    creativity = st.slider("Creativity", min_value=-0.09, max_value=0.09, value=0.00)
+    variety = st.slider("Variety", min_value=-0.09, max_value=0.09, value=0.00)
+
 # Checks if there is an input and if it works
 if(input!= ""):
     try:
+        difficulty = calculateNewDifficulty(editInput(input))
+        presentation = calculatePresentation(difficulty)
+        oldDifficulty = calculateOldDifficulty(editInput(input))
+        oldPresentation = calculatePresentation(oldDifficulty)
+            
+        st.write("Custom Presentation Score: ", round(calculateEditedPresentation(difficulty, entertainment, execution, musicality, creativity, variety), 2))
+
         col1, col2 = st.columns(2)
         with col1:
-            difficulty = calculateNewDifficulty(editInput(input))
-            presentation = calculatePresentation(difficulty)
             st.subheader("New Difficulty(4.0.0)")
             st.write("Difficulty: ", difficulty)
             data = pd.DataFrame(printOutput(presentation), columns=("Presentation Type", "Max", "Min"))
             st.dataframe(data, hide_index=True)
         with col2:
             st.subheader("Old Difficulty(3.0.0) for Reference")
-            oldDifficulty = calculateOldDifficulty(editInput(input))
-            oldPresentation = calculatePresentation(oldDifficulty)
             st.write("Difficulty: ", oldDifficulty)
             oldData = pd.DataFrame(printOutput(oldPresentation), columns=("Presentation Type", "Max", "Min"))
             st.dataframe(oldData, hide_index=True)
@@ -127,7 +138,7 @@ else:
     data = pd.DataFrame(printOutput(presentation), columns=("Presentation Type", "Max", "Min"))
     st.write("Difficulty: ", difficulty)
     st.dataframe(data, hide_index=True)
-    print(printOutput(presentation))
+
 st.header("Rule Change in Calculating Difficulty Score")
 st.write("The difficulty score will be the average of the power difficulty score, the wraps/releases difficulty score, and the multiples difficulty score.")
 st.write("This effectively changes the equation for calculating the difficulty of a trick from")
@@ -135,34 +146,44 @@ st.write("$\ D = 0.1*1.5^x $")
 st.write("to")
 st.write("$\ D = \dfrac{0.1*1.5^x}{3} $")
 st.write("As a result, these are the new point values for different levels compared to the old point values.")
-st.write('''
-| Level | Old Point Value | New Point Value|
-| ---  | --- | --- |
-| 0.5  | 0.12 | 0.04 |
-| 1 | 0.15 | 0.05 |   
-| 2 | 0.23 | 0.08 |   
-| 3 | 0.34 | 0.11 |   
-| 4 | 0.51 | 0.17 |   
-| 5 | 0.76 | 0.25 |   
-| 6 | 1.14 | 0.38 |   
-| 7 | 1.76 | 0.57 |   
-| 8 | 2.56 | 0.85 |   
-# Determining the Level of a Trick
+oldVNewPointValues = pd.DataFrame(
+                                [[0.5, 0.12, 0.04],
+                                  [1, 0.15, 0.05],
+                                  [2, 0.23, 0.08],
+                                  [3, 0.34, 0.11],
+                                  [4, 0.51, 0.17],
+                                  [5, 0.76, 0.25],
+                                  [6, 1.14, 0.38],
+                                  [7, 1.76, 0.57],
+                                  [8, 2.56, 0.85],], 
+                                  columns=("Level", "Old Point Value", "New Point Value"))
+st.dataframe(oldVNewPointValues, hide_index=True)
+st.header("Determining the Level of a Trick")
+st.subheader("Gymnastics and Power")
+gymnasticsAndPower = pd.DataFrame(
+                                    [
+                                        [1, " - Cartwheel <br> Roundoff <br> Forward/backward roll <br> Butterfly Kick (B kick)", 
+                                        "Standing to Frog/Push-up without pulling the rope <br> Frog/Push-up to standing without pulling the rope <br> Basic power skill entrance"],
+                                        [2, "Front Handspring <br> Kip <br> Dive roll", 
+                                        "Frog <br> Push-up <br> Crab <br> Split"],
+                                        [3, "Aerial <br> Barani <br> Back Handspring <br> ¾ flip (front ¾ flip landing in a crab position or back ¾ landing in a push-up position)", 
+                                        "One-handed power Frog/Push-up to standing <br> Frog from Two Feet(no revolutions of the rope) <br> Frog to single bounce cross landing in standing"],
+                                        [4, "Front Aerial <br> Front Flip <br> Back Flip <br> Butterfly Twist (B twist)", 
+                                        "Push-up to Pushup <br> Frog to Pushup <br> Punch Frog"],
+                                        [5, "Flips with twists (half or full) <br> Front flip with double under <br> ¾ flip with triple under", 
+                                        "One-handed Punch Frog <br> Double Under Frog <br> Push-Up to Push-Up or Belch with double under or cross"],
+                                        [6, "Flips with 1.5 or 2 Spins <br> Kip Whip <br> Front Handspring Whip <br> Backflip with Triple Under <br> Front Flip with Triple Under", 
+                                        "Split to backwards open single bounce landing in standing (must be full split with rope on the ground before pulling)"],
+                                        [7, "Back Flip with a TJ <br> Kip Whip with a Cross", 
+                                        "Sunny D <br> Darkside <br> Triple under landing in Frog"],
+                                        [8, "Double Back <br> Triple Full <br> Back Flip Triple Under with an AS Cross", 
+                                        "Moneymaker"]],
+                                        columns=("Level", "Gymnastics", "Power"))
 
-### Gymnastics and Power
+st.markdown(gymnasticsAndPower.to_html(index=False), unsafe_allow_html=True)
+st.write("")
 
-> | Level | Gymnastics | Power |
-> | ----- | -------- | -------- |
-> | 1     | Cartwheel, Roundoff, Forward/backward roll, Butterfly Kick (B kick) | Standing to Frog/Push-up without pulling the rope, Frog/Push-up to standing without pulling the rope, Basic power skill entrance |
-> | 2     | Front Handspring, Kip, Dive roll | Frog, Push-up, Crab, Split | 
-> | 3     | Aerial, Barani, Back Handspring, ¾ flip (front ¾ flip landing in a crab position or back ¾ landing in a push-up position)| One-handed power Frog/Push-up to standing, Frog from Two Feet(no revolutions of the rope), Frog to single bounce cross landing in standing |
-> | 4     | Front Aerial, Front Flip, Back Flip, Butterfly Twist (B twist) | Push-up to Pushup, Frog to Pushup, Punch Frog |
-> | 5     | Flips with twists (half or full), Front flip with double under, ¾ flip  with triple under  | One-handed Punch Frog, Double Under Frog, Push-Up to Push-Up or Belch with double under or cross |
-> | 6     | Flips with 1.5 or 2 Spins, Kip Whip, Front Handspring Whip, Backflip with Triple Under, Front Flip with Triple Under | Split to backwards open single bounce landing in standing (must be full split with rope on the ground before pulling) |
-> | 7     | Back Flip with a TJ, Kip Whip with a Cross  | Sunny D, Darkside, Triple under landing in Frog |
-> | 8     | Double Back, Triple Full, Back Flip Triple Under with an AS Cross | Moneymaker |
-> 
-### Gymnastics and Power Modifiers
+st.write("""### Gymnastics and Power Modifiers
 > - +1 for Cross performed with power or gymnastics skills and/or flips
 > 
 > - +1 to the level of a multiple landing in Push-up, Split or Crab position
@@ -177,20 +198,25 @@ st.write('''
 > 
 > - -1 for gymnastics with rope held with only one hand and not jumping the rope (for example, one-hand handspring (L2) with both handles in one hand (-1) = L1; front aerial (L3) with both handles in one hand (-1) = L2)
 >
---------------------------------------------------
-### Multiples and Rope Manipulation
-> | Level | Multiples | Rope Manipulation |
-> | ----- | -------- | ----- |
-> | 0.5   |          | Foot work (performed at a slow pace), Criss Cross, Basic arm wrap | 
-> | 1     | Double Under | Restricted side-swing, Toad, Crougar, EB, Basic rope release, Foot work (performed at a fast pace) |
-> | 2     | Triple Under, Double Under with One-arm Restriction | AS, CL, TS, Elephant toad, KN, EM, Caboose, Mic release, Crougar wrap |
-> | 3     | Quadruple Under, Triple Under TJ (Triple Under toad), Double Under AS, Double Under mic, Triple Under EK  | Lasso release caught in the air, Forward French Trick, One-arm restriction with a double wrap (For example, Toad jumped with double wrap), AS go-go/crazy-cross, Catching a mic release in a one arm restriction |
-> | 4     | Quintuple Under, Quadruple Under TJ, Triple Under EB TJ, Triple Under AS, Double Under AS×AS | Backward French-trick, Catching mic release in backward two-arm restriction |
-> | 5     | Sextuple Under, Quintuple Under EB, Quadruple Under CL, Hummingbird, Double Under AS switch TS, Double Under AS Switch CL |
-> | 6     | Quintuple Under AS, Triple Under AS CL TS, Quintuple EB open AS, Quadruple Under AS×AS, Backward Quadruple Under AS open, landing in AS |
-> | 7     | Quintuple Under with under-the-leg mic caught in one handed restricted position, Quintuple Under 360° with backward leg-over cross and forward leg-over cross |
-> | 8     | Quintuple with under-the-leg mic caught in a two handed restriction, Backwards quintuple under TS open CL open AS |
->
+""")
+multiplesAndRopeManipulation = pd.DataFrame(
+    [
+        [0.5, "", "Foot work (performed at a slow pace) <br> Criss Cross <br> Basic arm wrap"],
+        [1, "Double Under", "Restricted side-swing <br> Toad <br> Crougar <br> EB <br> Basic rope release <br> Foot work (performed at a fast pace)"],
+        [2, "Triple Under <br> Double Under with One-arm Restriction", "AS <br> CL <br> TS <br> Elephant toad <br> KN <br> EM <br> Caboose <br> Mic release <br> Crougar wrap"],
+        [3, "Quadruple Under <br> Triple Under TJ (Triple Under toad) <br> Double Under AS <br> Double Under mic <br> Triple Under EK", "Lasso release caught in the air <br> Forward French Trick <br> One-arm restriction with a double wrap (For example, Toad jumped with double wrap) <br> AS go-go/crazy-cross <br> Catching a mic release in a one arm restriction"],
+        [4, "Quintuple Under <br> Quadruple Under TJ <br> Triple Under EB TJ <br> Triple Under AS <br> Double Under AS×AS", "Backward French-trick <br> Catching mic release in backward two-arm restriction"],
+        [5, "Sextuple Under <br> Quintuple Under EB <br> Quadruple Under CL <br> Hummingbird <br> Double Under AS switch TS <br> Double Under AS Switch CL", ""],
+        [6, "Quintuple Under AS <br> Triple Under AS CL TS <br> Quintuple EB open AS <br> Quadruple Under AS×AS <br> Backward Quadruple Under AS open <br> landing in AS", ""],
+        [7, "Quintuple Under with under-the-leg mic caught in one handed restricted position <br> Quintuple Under 360° with backward leg-over cross and forward leg-over cross", ""],
+        [8, "Quintuple with under-the-leg mic caught in a two handed restriction <br> Backwards quintuple under TS open CL open AS", ""]
+    ],
+    columns=["Level", "Multiples", "Rope Manipulation"],
+)
+st.markdown(multiplesAndRopeManipulation.to_html(index=False), unsafe_allow_html=True)
+st.write("")
+st.write('''
+> 
 ### Multiples Modifiers
 > - +1 for body rotation more than 270° in twist or flip direction (for example, EK, BC, full twist)
 > 
